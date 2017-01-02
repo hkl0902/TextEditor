9b7eab02c1f0e7b7c51a1ab6a855ec54a8be3c9a
Hyunkyu Michael Lee <hyunkyu@berkeley.edu>
2017-01-02 11:58:28 -0800

    Adds file to collect all constants/functions related to AutoLayout

---
+++ MarkdownEditor/LayoutUtils.swift
@@ -0,0 +1,27 @@
+//
+//  LayoutConstants.swift
+//  MarkdownEditor
+//
+//  Created by Hyunkyu Lee on 1/2/17.
+//  Copyright © 2017 Hyunkyu Lee. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+struct EditorLayoutConstants {
+    var MIN_EDITOR_WINDOW_WIDTH = 1500
+    var MIN_EDITOR_WINDOW_HEIGHT = 1000
+    
+    
+    // For now the height is the same as the editor height but this may change in the future
+    var MIN_TEXT_EDITOR_VIEW_WIDTH = 500
+    var MIN_TEXT_EDITOR_VIEW_HEIGHT = 1000
+    
+    var MIN_SIDE_FOLDER_VIEW_WIDTH = 500    // When visible
+    var MIN_SIDE_FOLDER_VIEW_HEIGHT = 1000  // When visible
+    
+    var MIN_NOTES_VIEW_WIDTH = 500          // When visible
+    var MIN_NOTES_VIEW_HEIGHT = 1000        // When visible
+    
+}
